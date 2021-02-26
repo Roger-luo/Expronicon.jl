94752238e093c689984af55d685ddb80f0ef357b
Roger-Luo <rogerluo.rl18@gmail.com>
2021-02-26 18:49:17 -0500

    export error

--- src/analysis.jl
+++ src/analysis.jl
@@ -3,7 +3,7 @@ module Analysis
 using MLStyle
 using ..Types
 using ..Transform
-export is_kw_fn, split_function, split_call, split_struct,
+export AnalysisError, is_kw_fn, split_function, split_call, split_struct,
     split_struct_name, annotations, uninferrable_typevars
 
 struct AnalysisError <: Exception
