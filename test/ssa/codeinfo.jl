8fac23ad7dde39c12c684b3c9ff3139c62e967c7
Roger-Luo <rogerluo.rl18@gmail.com>
2021-03-08 00:24:34 -0500

    add a simple test

--- test/ssa/codeinfo.jl
+++ test/ssa/codeinfo.jl
@@ -2,6 +2,11 @@ using Test
 using Yuan.SSA
 
 
-ci = obtain_codeinfo(cos, (Float64, ))
+ci = code_lowered(cos, (Float64, ))[1]
+ci, typ = code_typed(cos, (Float64, ))[1]
+ir, typ = code_ircode(cos, (Float64, ))[1]
+ir
 
-obtain_const_or_stmt(SSAValue(50), ci)
+stmt, type = obtain_const_or_stmt(SSAValue(50), ci)
+@test ci.code[50] == stmt
+@test ci.ssavaluetypes[50] == type
