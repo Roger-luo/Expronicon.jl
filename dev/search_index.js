3fdd9d318729886b5b500643e06ceb5a0d2fd1d6
Documenter.jl <documenter@juliadocs.github.io>
2021-03-08 06:11:10 +0000

    build based on f27bfc1

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = YuanExpr","category":"page"},{"location":"#YuanExpr","page":"Home","title":"YuanExpr","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [YuanExpr]","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Exmonicon","category":"page"},{"location":"#Exmonicon","page":"Home","title":"Exmonicon","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Exmonicon]","category":"page"}]
 }
