7832522254fdfa1aa3962000fcb4f3e9b8d58cbc
Documenter.jl <documenter@juliadocs.github.io>
2021-03-08 05:50:41 +0000

    build based on 162df15

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = YuanExpr","category":"page"},{"location":"#YuanExpr","page":"Home","title":"YuanExpr","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [YuanExpr]","category":"page"}]
+}
