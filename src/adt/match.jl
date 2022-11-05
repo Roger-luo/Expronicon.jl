69c28ce9a3a9115e92d94184159b7dc64a0e2167
Rogerluo <rogerluo.rl18@gmail.com>
2022-11-05 04:42:38 -0400

    fix match (#26)

--- src/adt/match.jl
+++ src/adt/match.jl
@@ -11,9 +11,42 @@ function compile_adt_pattern(t, self, type_params, type_args, args)
         end
     end
 
+    # singleton is not dispatch to ::VariantType
+    # variant_kind(t) === :singleton
+    if variant_kind(t) === :call
+        uncall_call(t, self, args, kwargs, type_args)
+    else # variant_kind(t) === :struct
+        uncall_struct(t, self, args, kwargs, type_args)
+    end
+end
+
+function uncall_call(t, self, args, kwargs, type_args)
+    isempty(kwargs) || throw(ArgumentError("keyword arguments are not supported"))
+    patterns = Function[]
+    fieldtypes = variant_fieldtypes(t)
+    nfields = length(fieldtypes)
+    n_args = length(args)
+
+    @switch args begin
+        @case if nfields === n_args end # default constructor
+            append!(patterns, map(self, args))
+        @case if nfields > n_args end # partial constructor
+            last(args) == :(_...) || throw(ArgumentError("partial constructor must end with _..."))
+            append!(patterns, map(self, args[1:end-1]))
+        # @case [:(_...)]
+        @case _
+    end
+
+    ret = call_decons(t, patterns)
+    isempty(type_args) && return ret
+    return and([self(Expr(:(::), Expr(:curly, adt_type(t), type_args...))), ret])
+end
+
+function uncall_struct(t, self, args, kwargs, type_args)
     partial_field_names = Symbol[]
     patterns = Function[]
     all_field_names = variant_fieldnames(t)
+    nfields = length(all_field_names)
     n_args = length(args)
 
     @switch args begin
@@ -24,18 +57,16 @@ function compile_adt_pattern(t, self, type_params, type_args, args)
                 push!(partial_field_names, field_name)
                 push!(patterns, self(arg.args[2]))
             end
-        @case if length(all_field_names) === n_args end # default constructor
-            args = replace(args, :(_...) => :_) # only one left
+        @case if n_args ≤ nfields end # default constructor
+            if n_args < nfields
+                last(args) == :(_...) || throw(ArgumentError("partial constructor must end with _..."))
+            end
+
+            if last(args) == :(_...)
+                args = args[1:end-1]
+            end
             append!(patterns, map(self, args))
-            append!(partial_field_names, all_field_names)
-        @case [:(_...)]
-            partial_field_names = Symbol[]
-            patterns = Function[]
-        @case if length(args) == 0 end # kwargs pattern
-            # skip
-        @case if length(args) !== length(all_field_names) end
-            error("count of positional fields should be same as "*
-            "the fields: $(join(all_field_names, ", "))")
+            append!(partial_field_names, all_field_names[1:length(args)])
         @case _
     end
 
@@ -56,20 +87,42 @@ function compile_adt_pattern(t, self, type_params, type_args, args)
         end
     end
 
-    ret = struct_decons(adt_type(t), partial_field_names, patterns)
+    ret = struct_decons(t, partial_field_names, patterns)
     isempty(type_args) && return ret
-    return and([self(Expr(:(::), Expr(:curly, t, type_args...))), ret])
+    return and([self(:(::$(adt_type(t)))), ret])
 end
 
-function struct_decons(t, partial_fields, ps, prepr::AbstractString = repr(t))
+function call_decons(t, ps, prepr::AbstractString = repr(t))
     function tcons(_...)
-        t
+        adt_type(t)
     end
 
     comp = MLStyle.Record.PComp(prepr, tcons;)
+    mask = variant_masks(t)
+    types = variant_fieldtypes(t)
     function extract(sub::Any, i::Int, ::Any, ::Any)
         quote
-            $(xcall(Base, :getproperty, sub, QuoteNode(partial_fields[i])))
+            $(xcall(Base, :getfield, sub, mask[i]))::$(types[i])
+        end
+    end
+    MLStyle.Record.decons(comp, extract, ps)
+end
+
+
+function struct_decons(t, partial_fields, ps, prepr::AbstractString = repr(t))
+    function tcons(_...)
+        adt_type(t)
+    end
+
+    comp = MLStyle.Record.PComp(prepr, tcons;)
+
+    names = variant_fieldnames(t)
+    types = variant_fieldtypes(t)
+    mask = variant_masks(t)
+    function extract(sub::Any, i::Int, ::Any, ::Any)
+        idx = findfirst(isequal(partial_fields[i]), names)::Int
+        quote
+            $(xcall(Base, :getfield, sub, mask[idx]))::$(types[idx])
         end
     end
     MLStyle.Record.decons(comp, extract, ps)
