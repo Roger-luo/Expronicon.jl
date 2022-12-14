9849b54d135e5a3bfe3a8264841e1b3b00f1afde
Roger-luo <rogerluo.rl18@gmail.com>
2022-12-13 22:11:37 -0500

    rename old docs

--- docs.old/make.jl
+++ docs.old/make.jl
@@ -2,20 +2,14 @@ using MLStyle
 using Expronicon
 using Documenter
 using ANSIColoredPrinters
-using DocThemeIndigo
-
-indigo = DocThemeIndigo.install(Expronicon)
+using DocumenterMarkdown
 
 makedocs(;
     modules=[Expronicon],
     authors="Roger-luo <rogerluo.rl18@gmail.com> and contributors",
     repo="https://github.com/Roger-luo/Expronicon.jl/blob/{commit}{path}#{line}",
     sitename="Expronicon.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://Roger-luo.github.io/Expronicon.jl",
-        assets=String[indigo, "assets/default.css"],
-    ),
+    format=Markdown(),
     pages=[
         "Quick Start" => "index.md",
         "Case Study" => "case.md",
