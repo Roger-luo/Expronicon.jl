869d17bda3a559e641bc7ef10bc85ac34e6ae645
a <a@a.a>
2024-07-18 16:25:59 +0200

    add tests

--- test/analysis.jl
+++ test/analysis.jl
@@ -169,6 +169,14 @@ end
     @test jlf.whereparams == Any[:T]
     @test jlf.args == Any[:(x::T), :y]
     @test jlf.rettype == :T
+
+    ex = quote
+        "foo $bar" 
+        f(x) = x+1
+    end
+
+    jlf = JLFunction(ex)
+    @test jlf.doc == Expr(:string, "foo ", bar)
 end
 
 @testset "JLStruct(ex)" begin
